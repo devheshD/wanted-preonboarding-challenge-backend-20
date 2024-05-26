bfd32ba00a349e0d162b260e3e2beaa637b10d36
rasung-ki <rlfktjd!8624>
2024-05-26 22:50:54 +0900

    Init project

---
+++ src/main/kotlin/com/devhesh/wantedmarket/WantedMarketApplication.kt
@@ -0,0 +1,11 @@
+package com.devhesh.wantedmarket
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WantedMarketApplication
+
+fun main(args: Array<String>) {
+    runApplication<WantedMarketApplication>(*args)
+}
---
+++ src/test/kotlin/com/devhesh/wantedmarket/WantedMarketApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.devhesh.wantedmarket
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WantedMarketApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
